110a8c8b5b01822a570fdd546a835a3d8a17afa3
PedroLucasAraujo <pedrolpriv@gmail.com>
2024-12-05 12:03:11 -0300

    feat: create home visual structure

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,36 @@
+import { styled, Heading, Text } from "@ignite-ui/react";
+
+export const Container = styled("div", {
+  maxWidth: "calc(100vw - (100vw - 1160px) / 2)",
+  height: "100vh",
+  marginLeft: "auto", // Joga elemento totalmente para direita quando o width for menor
+  display: "flex",
+  alignItems: "center",
+  gap: "$20", // 5rem
+});
+
+export const Hero = styled("div", {
+  maxWidth: 480,
+  padding: "0 $10",
+
+  [`> ${Heading}`]: {
+    "@media(max-width: 600px)": {
+      fontSize: "$6xl",
+    },
+  },
+
+  [`> ${Text}`]: {
+    "@media(max-width: 600px)": {
+      fontSize: "$xl",
+    },
+  },
+});
+
+export const Preview = styled("div", {
+  paddingRight: "$8",
+  overflow: "hidden",
+
+  "@media(max-width: 600px)": {
+    display: "none",
+  },
+});
